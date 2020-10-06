3193dda402f6f933ce6bcc5ca64add51580ed885
Ankit <ankitkumarmbill1@gmail.com>
2020-10-07 00:20:42 +0530

    added some code

---
+++ Snake Game/engine.js
@@ -0,0 +1,4 @@
+canvas = document.getElementById("canvas");
+canvas.style.height = "100vh";
+canvas.style.width = "100vw";
+// canvas.style.background - image: `url("C:\Users\KIIT\Desktop\Practice\Projects\Web-dev\javascript\Snake Game")`;
